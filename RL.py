2834d8d42e3f0aec8dcf5fbf7ccf4d2a46b59bc1
Phillip <phillipables1@gmail.com>
2018-03-01 21:56:48 -0500

    Reinforcement learning page with tensorflow cv2 pong numpy random and deque imported

---
+++ RL.py
@@ -0,0 +1,7 @@
+  # our reinforcement learning with tensor flow
+import tensorflow as tf
+import cv2  # open cv helps us format our pixel data better for reading in tensor flow
+import pong
+import numpy as np  # which is going to help us with math
+import random
+from collections import deque
